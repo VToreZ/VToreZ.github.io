1eb72349a91f94de11e887575f6760864fd0a5e4
VToreZ <vtorez@gmail.com>
2020-09-08 15:52:44 +0300

    chat deploy

---
+++ demochat/precache-manifest.7a2e3d2f252385ca7a88bf8438596473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993d19fbcb508787ca81a4ae2f76b364",
+    "url": "/demochat/index.html"
+  },
+  {
+    "revision": "2f52859c7d107d8b71d0",
+    "url": "/demochat/static/css/main.02f2df5c.chunk.css"
+  },
+  {
+    "revision": "12ede928140fa882def6",
+    "url": "/demochat/static/js/2.c7632595.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demochat/static/js/2.c7632595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f52859c7d107d8b71d0",
+    "url": "/demochat/static/js/main.fb90ea84.chunk.js"
+  },
+  {
+    "revision": "e561b8a0c767c2f7962f",
+    "url": "/demochat/static/js/runtime-main.a53ea302.js"
+  }
+]);
