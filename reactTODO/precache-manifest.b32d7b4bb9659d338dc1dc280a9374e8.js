ec31b2eb21e2cf08ce4f23ac826113f4799103d6
VToreZ <vtorez@gmail.com>
2020-09-09 17:08:03 +0300

    reactTODO added

---
+++ reactTODO/precache-manifest.b32d7b4bb9659d338dc1dc280a9374e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ebae236e760a3102c0cb8f5a2007a3",
+    "url": "/reactTODO/index.html"
+  },
+  {
+    "revision": "68ee94a817d3e46f0b24",
+    "url": "/reactTODO/static/css/main.66ab013f.chunk.css"
+  },
+  {
+    "revision": "85c5c8301dee71ac8896",
+    "url": "/reactTODO/static/js/2.f7551099.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTODO/static/js/2.f7551099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ee94a817d3e46f0b24",
+    "url": "/reactTODO/static/js/main.da280981.chunk.js"
+  },
+  {
+    "revision": "97cb1a8fca7e01396364",
+    "url": "/reactTODO/static/js/runtime-main.48dae87a.js"
+  }
+]);
